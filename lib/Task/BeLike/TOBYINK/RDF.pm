68ec53dbaee9c2cea08b19718e02799e32ffde17
Toby Inkster <tobyink@cpan.org>
2012-05-29 22:16:07 +0000

    Import of TOBYINK/Task-BeLike-TOBYINK-RDF-2012.150 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-TOBYINK-RDF
    gitpan-cpan-version:      2012.150
    gitpan-cpan-path:         TOBYINK/Task-BeLike-TOBYINK-RDF-2012.150.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/TOBYINK/RDF.pm
@@ -0,0 +1,69 @@
+package Task::BeLike::TOBYINK::RDF;
+
+use 5.010;
+
+BEGIN {
+	$Task::BeLike::TOBYINK::RDF::AUTHORITY = 'cpan:TOBYINK';
+	$Task::BeLike::TOBYINK::RDF::VERSION   = '2012.150';
+}
+
+__PACKAGE__
+__END__
+
+=head1 NAME
+
+Task::BeLike::TOBYINK::RDF - be like TOBYINK and use these RDF stuffz
+
+=head1 DESCRIPTION
+
+Task installs the following modules:
+
+=over
+
+=item * RDF::NS
+
+=item * RDF::Query
+
+=item * RDF::Query::Client
+
+=item * RDF::QueryX::Lazy
+
+=item * RDF::RDB2RDF
+
+=item * RDF::RDFa::Generator
+
+=item * RDF::RDFa::Parser
+
+=item * RDF::TriN3
+
+=item * RDF::Trine
+
+=item * Test::RDF
+
+=item * XML::Atom::FromOWL
+
+=item * XML::Atom::OWL
+
+=back
+
+=head1 SEE ALSO
+
+L<Acme::About::TOBYINK>,
+L<Task::BeLike::TOBYINK>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
